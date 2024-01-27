bc4fcc9ccaaf42a36bc3e064e7baf9c8352048fa
RixvNX <142212639+RixvNX@users.noreply.github.com>
2024-01-27 16:12:12 +0800

    The Test beta0002t02

--- _applications/com/raise/system/CommandLine.app/bin/Api.py
+++ _applications/com/raise/system/CommandLine.app/bin/Api.py
@@ -1,5 +1,45 @@
+from bin import Paths
 from bin import FinalValue
 from bin import Files
+import os
 
-def get(__ApplicationName:str, __APIName:str, __Args:list):
-    __APP = Files._readfile(f"{FinalValue.__ROOTDIR__}/_registry/applications/point/{__ApplicationName}.item", True)
+class _Ergodic:
+    def _Files(__Path:str, __absolutePath:bool=False) -> list:
+        __ErgodicResult = []
+        if __absolutePath == True:
+            for root, dirs, files in os.walk(f"{FinalValue.__ROOTDIR__}/{__Path}", topdown=False):
+                __ErgodicResult = Files
+        else:
+            for root, dirs, files in os.walk(f"{Paths.getSubPaths()}/{__Path}", topdown=False):
+                __ErgodicResult = Files
+        return __ErgodicResult
+    
+    def _Dirs(__Path:str, __absolutePath:bool=False) -> list:
+        __ErgodicResult = []
+        if __absolutePath == True:
+            for root, dirs, files in os.walk(f"{FinalValue.__ROOTDIR__}/{__Path}", topdown=False):
+                __ErgodicResult = dirs
+        else:
+            for root, dirs, files in os.walk(f"{Paths.getSubPaths()}/{__Path}", topdown=False):
+                __ErgodicResult = dirs
+        return __ErgodicResult
+
+
+def get(__ApplicationName:str, __APIName:str, __Args:list) -> dict:
+    """
+    Get application API functions, class or variables.
+    """
+    try:
+        __APPPotiner = Files._readfile(f"{FinalValue.__ROOTDIR__}/_registry/applications/point/{__ApplicationName}.item", True)
+        __APP = eval(__APPPotiner)["value"]
+        __APPAPIS = _Ergodic._Files(f"{__APP}", True)
+        if __APPAPIS == [] or __APPAPIS == [""]:
+            return {"status":True, "value":""}
+        elif __APIName not in __APPAPIS:
+            return {"status":False, "value":"NoAPIException"}
+        else:
+            return {"status":False, "value":Files._readfile(__APP+'/'+__APIName+'.api', True)}
+    except SyntaxError:
+        return {"status":False, "value":"RegistryItemException"}
+    except KeyError:
+        return {"status":False, "value":"RegistryItemException"}
